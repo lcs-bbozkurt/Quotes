aa8e0b0139b76ebbbd9fea555e2af1f3dad8ee39
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-02-28 13:43:47 -0500

    I can load the quotes using the the "loadQuotes" func.

--- Quotes/Views/ContentView.swift
+++ Quotes/Views/ContentView.swift
@@ -123,95 +123,95 @@ struct ContentView: View {
         }
     }
     
-    
-}
-
-// MARK: Functions
-func loadNewQuote() async {
-    // Assemble the URL that points to the endpoint
-    let url = URL(string: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en")!
-    
-    // Define the type of data we want from the endpoint
-    // Configure the request to the web site
-    var request = URLRequest(url: url)
-    // Ask for JSON data
-    request.setValue("application/json",
-                     forHTTPHeaderField: "Accept")
-    
-    // Start a session to interact (talk with) the endpoint
-    let urlSession = URLSession.shared
-    
-    // Try to fetch a new joke
-    // It might not work, so we use a do-catch block
-    do {
+    // MARK: Functions
+    func loadNewQuote() async {
+        // Assemble the URL that points to the endpoint
+        let url = URL(string: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en")!
         
-        // Get the raw data from the endpoint
-        let (data, _) = try await urlSession.data(for: request)
+        // Define the type of data we want from the endpoint
+        // Configure the request to the web site
+        var request = URLRequest(url: url)
+        // Ask for JSON data
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Accept")
         
-        // Attempt to decode the raw data into a Swift structure
-        // Takes what is in "data" and tries to put it into "currentJoke"
-        //                                 DATA TYPE TO DECODE TO
-        //                                         |
-        //                                         V
-        currentQuote = try JSONDecoder().decode(Quote.self, from: data)
+        // Start a session to interact (talk with) the endpoint
+        let urlSession = URLSession.shared
         
-        // Reset the flag that tracks whether the current joke
-        // is a favourite
-        //f currentJokeAddedToFavourites = false
+        // Try to fetch a new joke
+        // It might not work, so we use a do-catch block
+        do {
+            
+            // Get the raw data from the endpoint
+            let (data, _) = try await urlSession.data(for: request)
+            
+            // Attempt to decode the raw data into a Swift structure
+            // Takes what is in "data" and tries to put it into "currentJoke"
+            //                                 DATA TYPE TO DECODE TO
+            //                                         |
+            //                                         V
+            currentQuote = try JSONDecoder().decode(Quote.self, from: data)
+            
+            // Reset the flag that tracks whether the current joke
+            // is a favourite
+            //f currentJokeAddedToFavourites = false
+            
+        } catch {
+            print("Could not retrieve or decode the JSON from endpoint.")
+            // Print the contents of the "error" constant that the do-catch block
+            // populates
+            print(error)
+        }
         
-    } catch {
-        print("Could not retrieve or decode the JSON from endpoint.")
-        // Print the contents of the "error" constant that the do-catch block
-        // populates
-        print(error)
+    }
+    func persistQuotes() {
+        // Get a location under which to save the data
+        let filename = getDocumentsDirectory().appendingPathComponent(savedQuotesLabel)
+        print(filename)
+        // Try to encode the data in our list of favourites to JSON
+        do {
+            // Create a JSONEncoder()
+            let encoder = JSONEncoder()
+            // Configured the encoder to "pretty print" the JSON
+            encoder.outputFormatting = .prettyPrinted
+            // encode the list of favourites we have collect
+            let data = try encoder.encode(favourites)
+            // Write the JSON to a file in the filename location we came up with earlier.
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+            // See the data that was written
+            print("Saved data to the Documents directory successfully.")
+            print("=========")
+            print(String(data: data, encoding: .utf8)!)
+        } catch {
+            print("Unable to write list of favourites to the Documents directory")
+            print("=======")
+            print(error.localizedDescription)
+        }
+    }
+    func loadQuotes() {
+        // Get a location under which to save the data
+        let filename = getDocumentsDirectory().appendingPathComponent(savedQuotesLabel)
+        print(filename)
+        // Attempt to load the data
+        do {
+            // load the row data
+            let data = try Data(contentsOf: filename)
+            // See the data that was written
+            print("Saved data to the Documents directory successfully.")
+            print("=========")
+            print(String(data: data, encoding: .utf8)!)
+            // Decode the JSON into Swift native data structures.
+            // NOTE: We use [Quote] since we are loading into a list (array)
+            favourites = try JSONDecoder().decode([Quote].self, from: data)
+        } catch {
+            // What went wrong
+            print("Could not load the data from the JSON file")
+            print("====")
+            print(error.localizedDescription)
+        }
     }
     
-}
-func persistQuotes() {
-    // Get a location under which to save the data
-    let filename = getDocumentsDirectory().appendingPathComponent(savedQuotesLabel)
-    print(filename)
-    // Try to encode the data in our list of favourites to JSON
-    do {
-        // Create a JSONEncoder()
-        let encoder = JSONEncoder()
-        // Configured the encoder to "pretty print" the JSON
-        encoder.outputFormatting = .prettyPrinted
-        // encode the list of favourites we have collect
-        let data = try encoder.encode(quotes)
-        // Write the JSON to a file in the filename location we came up with earlier.
-        try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
-        // See the data that was written
-        print("Saved data to the Documents directory successfully.")
-        print("=========")
-        print(String(data: data, encoding: .utf8)!)
-    } catch {
-        print("Unable to write list of favourites to the Documents directory")
-        print("=======")
-        print(error.localizedDescription)
-    }
-}
-func loadQuotes() {
-    // Get a location under which to save the data
-    let filename = getDocumentsDirectory().appendingPathComponent(savedQuotesLabel)
-    print(filename)
-    // Attempt to load the data
-    do {
-        // load the row data
-        let data = try Data(contentsOf: filename)
-        // See the data that was written
-        print("Saved data to the Documents directory successfully.")
-        print("=========")
-        print(String(data: data, encoding: .utf8)!)
-        // Decode the JSON into Swift native data structures.
-        // NOTE: We use [DadJoke] since we are loading into a list (array)
-        Quotes = try JSONDecoder().decode([Quote].self, from: data)
-    } catch {
-        // What went wrong
-        print("Could not load the data from the JSON file")
-        print("====")
-        print(error.localizedDescription)
-    }
+    
 }
 
 
